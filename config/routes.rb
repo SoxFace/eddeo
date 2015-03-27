74919ca0cd93ef51e8a6225598a59be8793bcf77
soxface <sonyacorcoran@gmail.com>
2015-03-27 15:34:38 +1100

    layout for feature video page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
   root :to => "videos#index"
+
+  get "featured", to: "videos#featured", as: "featured"
+
+  # GET REQUEST TO VIMEO API
   get '/populate' => "videos#populate"
 end
