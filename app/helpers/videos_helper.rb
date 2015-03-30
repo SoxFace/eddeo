e064b45f75533409ec15c97363578a0c5b95f8db
soxface <sonyacorcoran@gmail.com>
2015-03-30 11:28:07 +1100

    renamed video helpers to match vimeo database name

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -8,53 +8,53 @@ module VideosHelper
 
   def lower_limb
     lower_limb = 
-      [{:name=>"Great Toe Positioning: AP (Dorsiplantar)"},
+      [{:name=>"Great Toe Positioning: AP"},
       {:name=>"Great Toe Positioning: Lateral"},
-      {:name=>"Toes Positioning: AP (Dorsiplantar)"},
+      {:name=>"Toes Positioning: AP"},
       {:name=>"Toes Positioning: Oblique"},
       {:name=>"Foot Positioning: DP"},
       {:name=>"Foot Positioning: Oblique"},
       {:name=>"Foot Positioning: Lateral"},
-      {:name=>"Foot Positioning: Weight Bearing DP"},
-      {:name=>"Foot Positioning: Weight Bearing Lateral"},
+      {:name=>"Foot Positioning: DP Weight"},
+      {:name=>"Foot Positioning: Lateral Weight"},
       {:name=>"Ankle Positioning: AP"},
       {:name=>"Ankle Positioning: AP (Mortice)"},
-      {:name=>"Ankle Positioning: Medial Oblique"},
+      {:name=>"Ankle Positioning: Medial"},
       {:name=>"Ankle Positioning: Lateral"},
-      {:name=>"Tib / Fib Positioning: AP"},
+      {:name=>"Tib/Fib Positioning: AP"},
       {:name=>"Tib / Fib Positioning: Lateral"},
       {:name=>"Knee Positioning: AP"},
       {:name=>"Knee Positioning: Lateral"},
-      {:name=>"Knee Positioning: Lateral HBL"},
-      {:name=>"Knee Positioning: Intercondylar Notch"},
-      {:name=>"Knee Positioning: Skyline Patella"},
+      {:name=>"Knee Positioning: HBL"},
+      {:name=>"Knee Positioning: Inter"},
+      {:name=>"Knee Positioning: Skyline"},
       {:name=>"Femur Positioning: AP"},
-      {:name=>"Femur Positioning: AP (Hip Down)"},
-      {:name=>"Femur Positioning: Lateral (Knee Up)"},
-      {:name=>"Femur Positioning: Lateral (Hip Down)"}]
+      {:name=>"Femur Positioning: AP (Hip)"},
+      {:name=>"Femur Positioning: Lateral"},
+      {:name=>"Femur Positioning: Lateral (Hip)"}]
     find_videos(lower_limb)
   end
 
   def chest
     chest =
-      [{:name=>"Chest Positioning: PA Erect (Inspiration)"},
-      {:name=>"Chest Positioning: PA Erect (Expiration)"},
+      [{:name=>"Chest Positioning: PA Ins"},
+      {:name=>"Chest Positioning: PA Exp"},
       {:name=>"Chest Positioning: Lateral"},
-      {:name=>"Chest Positioning: AP (Seated)"}]
+      {:name=>"Chest Positioning: Chest AP"}]
     find_videos(chest)
   end
 
   def shoulder
     shoulder = 
       [{:name=>"Humerus Positioning: AP"},
-      {:name=>"Humerus Positioning: Lateral AP"},
-      {:name=>"Humerus Positioning: Stop Sign Lateral"},
-      {:name=>"Shoulder Positioning: AP (Humerus Neutral)"},
-      {:name=>"Shoulder Positioning: Lateral (15 Degrees Caudal)"},
+      {:name=>"Humerus Positioning: Lateral"},
+      {:name=>"Humerus Positioning: Stop Sign"},
+      {:name=>"Shoulder Positioning: AP"},
+      {:name=>"Shoulder Positioning: Lateral 15"},
       {:name=>"Shoulder Positioning: Lateral"},
-      {:name=>"Shoulder Positioning: AP Oblique (15 Degrees Caudal Internal Rotation)"},
-      {:name=>"Shoulder Positioning: AP Oblique (15 Degrees Caudal External Rotation)"},
-      {:name=>"Shoulder Positioning: Supero-Inferior"},
+      {:name=>"Shoulder Positioning: Oblique Internal"},
+      {:name=>"Shoulder Positioning: Oblique External"},
+      {:name=>"Shoulder Positioning: Supero"},
       {:name=>"Clavicle Positioning: PA"},
       {:name=>"Clavicle Positioning: PA (15 Degrees Caudal)"},
       {:name=>"Clavicle Positioning: AP"},
@@ -65,40 +65,40 @@ module VideosHelper
   def pelvis
     pelvis = 
       [{:name=>"Pelvis Positioning: AP"},
-      {:name=>"Hip Positioning: Right Downside (Judet View)"},
-      {:name=>"Hip Positioning: Left Upside (Judet View)"},
-      {:name=>"Hip Positioning: Single Hip AP"},
+      {:name=>"Hip Positioning: Judet Right Down"},
+      {:name=>"Hip Positioning: Judet Left Up"},
+      {:name=>"Hip Positioning: AP"},
       {:name=>"Hip Positioning: Oblique"},
-      {:name=>"Hip Positioning: Horizontal Beam Lateral"},
-      {:name=>"Abdomen Positioning: Supine / KUB"},
+      {:name=>"Hip Positioning: Horizontal Beam"},
+      {:name=>"Abdomen Positioning: Supine"},
       {:name=>"Abdomen Positioning: Bladder"},
-      {:name=>"Abdomen Positioning: Left Decubitous"},
-      {:name=>"Abdomen Positioning: PA Erect"}]
+      {:name=>"Abdomen Positioning: Decubitous"},
+      {:name=>"Abdomen Positioning: PA"}]
     find_videos(pelvis)
   end
 
   def spinal
     spinal = 
-      [{:name=>"Cervical Spine Positioning: Lateral (Neutral)"},
-      {:name=>"Cervical Spine Positioning: Lateral (Swimmers)"},
+      [{:name=>"Cervical Spine Positioning: Lateral"},
+      {:name=>"Cervical Spine Positioning: Swimmers"},
       {:name=>"Cervical Spine Positioning: AP Odontoid"},
       {:name=>"Cervical Spine Positioning: AP"},
-      {:name=>"Cervical Spine Positioning: Right Anterior Oblique"},
-      {:name=>"Cervical Spine Positioning: Left Anterior Oblique"},
-      {:name=>"Cervical Spine Positioning: Right Posterior Oblique"},
-      {:name=>"Cervical Spine Positioning: Left Posterior Oblique"},
-      {:name=>"Thoracic Spine Positioning: AP (Supine)"},
-      {:name=>"Thoracic Spine Positioning: Lateral (On Table Breathing Technique)"},
+      {:name=>"Cervical Spine Positioning: Oblique RA"},
+      {:name=>"Cervical Spine Positioning: Oblique LA"},
+      {:name=>"Cervical Spine Positioning: Oblique RP"},
+      {:name=>"Cervical Spine Positioning: Oblique LP"},
+      {:name=>"Thoracic Spine Positioning: AP"},
+      {:name=>"Thoracic Spine Positioning: Lateral"},
       {:name=>"Lumbar Spine AP"},
-      {:name=>"Lumbar Spine L5/S1 Junction AP (15 Degrees Cranial Angulation)"},
+      {:name=>"Lumbar Spine L5 AP"},
       {:name=>"Lumbar Spine Lateral"},
-      {:name=>"Lumbar Spine L5/S1 Junction Lateral"},
-      {:name=>"Lumbar Spine Left Posterior Oblique"},
-      {:name=>"Lumbar Spine Right Posterior Oblique"},
+      {:name=>"Lumbar Spine L5 Lateral"},
+      {:name=>"Lumbar Spine Left Oblique"},
+      {:name=>"Lumbar Spine Right Oblique"},
       {:name=>"SI Joint Positioning: Left Oblique"},
       {:name=>"SI Joint Positioning: Right Oblique"},
-      {:name=>"Sacrum Positioning: AP (15 Degrees Cranial Angulation)"},
-      {:name=>"Coccyx Positioning: AP (15 Degrees Cranial Angulation)"},
+      {:name=>"Sacrum Positioning: AP"},
+      {:name=>"Coccyx Positioning: AP"},
       {:name=>"Sacrum & Coccyx Positioning: Lateral"}]
     find_videos(spinal)
   end
