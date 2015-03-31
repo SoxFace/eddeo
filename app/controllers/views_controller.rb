ba0923ff66a72a4b38f9efa09cc1991f1a24371e
soxface <sonyacorcoran@gmail.com>
2015-03-31 13:55:18 +1100

    tracking only uniq views

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,6 +1,6 @@
 class ViewsController < ApplicationController
 
   def create
-    @view = View.create( :user_id => current_user.id, :video_id => params["video_id"] )
+    @view = View.find_or_create_by( :user_id => current_user.id, :video_id => params["video_id"] )
   end
 end
